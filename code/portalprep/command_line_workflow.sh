f96e5c0fe0f6d9039431900a0e27bebf5d15839a
Kelly Kearney <kelly.kearney@noaa.gov>
2026-02-24 10:28:28 -0800

    Beginning overhaul for more Portal-like data format

--- code/portalprep/command_line_workflow.sh
+++ code/portalprep/command_line_workflow.sh
@@ -10,7 +10,6 @@
 # Run on GFDL PPAN analysis cluster:
 
 # ./extract_mom6nep_selected.sh 1 254 447 743 1993 2024 /archive/e1n/fre/cefi/NEP/2024_11/NEP_nudge_spinup/gfdl.ncrc6-intel23-repro/history mom6nep_hc202411
-
 # Run on UW Hyak-klone:
 # (Note: script was originally hard-coded for the mom6nep_hc202411 simulation and klone file structure, and included forecast calculation. 
 #  This call is no longer forward-compatible with the newer version)
@@ -29,5 +28,32 @@
 # Run on UW Hyak-klone:
 
 # ./calculate_clim_anom.sh mom6nep_hc202507 /gscratch/cicoes/GR011846_reem/CEFI_data
-./calculate_persis_forecast.sh 20250101 101 mom6nep_hc202507 /gscratch/cicoes/GR011846_reem/CEFI_data
+# ./calculate_persis_forecast.sh 20250101 101 mom6nep_hc202507 /gscratch/cicoes/GR011846_reem/CEFI_data
+
+#--------------------
+# Updated
+# Summer ESR 2025
+#--------------------
+
+# Run on GFDL PPAN analysis cluster:
+
+# ./subset_from_archive.sh --region nep \
+#                          --subdomain 0 342 446 743 \
+#                          --years 1993 2025 \
+#                          --mmdd 0101 \
+#                          --archdir /archive/e1n/fre/cefi/NEP/2025_07/NEP10k_202507_physics_bgc/gfdl.ncrc6-intel23-repro/history \
+#                          --release e202507 \
+#                          ocean_daily tos,tob \
+#                          ocean_cobalt_daily_2d btm_o2,btm_co3_sol_arag,btm_htotal,btm_co3_ion,pco2surf \
+#                          ice_daily siconc
+
+./subset_from_archive.sh --region nep \
+                         --subdomain 0 342 446 743 \
+                         --years 2025 2025 \
+                         --mmdd 0701 \
+                         --archdir /archive/e1n/fre/cefi/NEP/2025_07/NEP10k_202507_physics_bgc/gfdl.ncrc6-intel23-repro/history \
+                         --release e202507 \
+                         ocean_daily tos,tob \
+                         ocean_cobalt_daily_2d btm_o2,btm_co3_sol_arag,btm_htotal,btm_co3_ion,pco2surf \
+                         ice_daily siconc
 
---
+++ code/portalprep/subset_from_archive.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+USEAGE="Usage: ./subset_from_archive.sh [--subdomain <iq1 iq2 jq1 jq2>]
+        [--years <yrstr yrend>] [--mmdd <mmdd>] [--archdir <arch_dir>]
+        [ftype1 <varstr1> ...] [-h]  
+
+where
+
+  --subdomain <iq1 iq2 jq1 jq2>: subdomain range, with grid variables 
+        extracted in the horizontal subregion defined by iq=iq1:iq2, 
+        jq = jq11:jq2 (ih = iq1+0.5:iq2-0.5, jh=iq1+0.5:iq2-0.5)
+        The specified subdomain must encompass at least one h-point,
+        i.e., iq2-iq1>0 and jq2-jq1>0.
+
+  --years <yrstr yrend>: years to process, indicated by starting and ending 
+        years of the desired range.  The script expects to find yearly-chunked 
+        output files identified by start date (YYYYMMDD); data will be 
+        identified by this naming scheme rather than by file contents.
+    
+  --mmdd <mmdd>: month and day associated with each archive file start date.  
+        Default of 0101 applies to most yearly-chunked data.
+
+  --archfdir <arch_dir>: path to archive folder where tarred dataset is found
+        (without trailing slash)
+
+  --ppdir <ppfol>: path to processed data folder where output files will be 
+        placed.  This will be the current directory unless otherwise specified.
+
+  --ftypeX varstrX: file type (corresponding to a file_name in the MOM6 diag 
+        table) and comma-delimited list of variables to extract from each file 
+        type.  For example 'ocean_daily tob,tos' will extract tob and tos 
+        variables from the MMDDYYYY.ocean_daily.nc files.  Any number of 
+        ftype/varstr pairs can be passed as input.
+
+  --region <region>: regional abbreviation that applies to the extracted data, 
+        used for file naming only
+
+  --release <release>: release abbreviation (or other preferred simulation name)
+        that applies to the extracted data, used for file naming only
+
+This function extracts a subset of variables across the indicated horizontal 
+subregion from a MOM6 simulation archive. The resulting files follow the 
+following naming scheme, which loosely mimics the CEFI Data Portal conventions:
+
+<ppdir>/<ftype>.<region>.<subdomain>.hcast.<release>.YYYYMMDD.nc
+"
+
+#--------------------
+# Setup
+#--------------------
+
+# Defaults
+
+mmdd="0101"
+ppfol="."
+
+# Input parsing
+
+ftype=()
+varstr=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --subdomain)
+      iq1=$2
+      iq2=$3
+      jq1=$4
+      jq2=$5
+      shift # past argument
+      shift # past value
+      shift
+      shift
+      shift
+      ;;
+    --years)
+      yrstr=$2
+      yrend=$3 
+      shift # past argument
+      shift # past value
+      shift
+      ;;    
+    --mmdd)
+      mmdd=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --ppdir)
+      ppfol=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --region)
+      region=$2
+      shift
+      shift
+      ;;
+    --release)
+      release=$2
+      shift
+      shift
+      ;;
+    -h|--help) 
+       echo "$USEAGE"
+       exit
+       ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      ftype+=("$1") # save positional arg
+      varstr+=("$2")
+      shift # past argument
+      shift
+      ;;
+  esac
+done
+
+#--------------------
+# Extract
+#--------------------
+
+# Subgrid details (note: using 0-based default option for ncks)
+#
+#   q: iq1-iq2, jq1-jq2
+#   h: iq1-(iq2-1), jq1-(jq2-1)
+#   u: iq1-iq2, jq1-jq2
+#   v: iq1-iq2, jq1-jq2
+#
+# The smallest "block" (jq2-jq1==1 and iq2-iq1==1) includes 1 h-point, 4 q-points,
+# 2 u-points, and 2 v-points.  For simplicity, I'm not allowing subsets that
+# don't include at least one h-point.
+
+if [-z ${iq1}] ; then
+    subsetflag=0
+    subdomainstr="full"
+elif [(((iq2-iq1)) < 1 ) || (((jq2-jq1)) < 1 )] ; then
+    echo "Subregion must span at least one h-point" >&2
+    exit 1
+else
+    subdomainstr="iq${iq1}-${iq2}jq${jq1}-${jq2}"
+    subsetflag=1
+fi
+
+for (( yr=$yrstr; yr<=$yrend; yr++ )); do
+
+    echo $yr
+
+    for ((i=0;i<${#ftype[@]};i++)); do
+
+        echo "   extracting variables from ${ftype[$i]}..."
+
+        # Untar from archive
+
+        tar -xf $arch_dir/${yr}${mmdd}.nc.tar ./${yr}${mmdd}.${ftype[$i]}.nc
+    
+        # Rename dimensions as needed (note: this does not change the values 
+        # of any coordinate variables associated with the dimensions!)
+
+        ncrename -d xT,ih  -d yT,jh \
+                 -d xTe,iq -d yTe,jq \
+                 -d xh,ih  -d yh,jh \
+                 -d xq,iq  -d yq,jq ./${yr}${mmdd}.${ftype[$i]}.nc
+
+        # Extract relevant variables using the specified regional subset
+
+        if [subsetflag -eq 1]; then
+            ncks -O -d iq,${iq1},${iq2} \
+                    -d jq,${jq1},${jq2} \
+                    -d ih,${iq1},${((iq2-1))} \
+                    -d jh,${jq1},${((jq2-1))} \
+                    -v ${varstr[$i]} \
+                    ${yr}${mmdd}.${ftype[$i]}.nc \
+                    ${ppfol}/${ftype[$i]}.${region}.${subdomainstr}.hcast.${release}.${yr}${mmdd}.nc
+        else
+            ncks -O -v ${varstr[$i]} \
+                    ${yr}${mmdd}.${ftype[$i]}.nc \
+                    ${ppfol}/${ftype[$i]}.${region}.${subdomainstr}.hcast.${release}.${yr}${mmdd}.nc
+        fi
+
+        # Delete untarred original (cleanup), move new file to 
+
+        rm ${yr}${mmdd}.${ftype[$i]}.nc
+
+        # # COBALT daily
+
+        # echo "   extracting COBALT variables..."
+        # tar -xf $arch_dir/${yr}${mmdd}.nc.tar ./${yr}${mmdd}.ocean_cobalt_daily_2d.nc
+        # ncks -F -A -d ih,${x1},${x2} -d jh,${y1},${y2} -v ${varc} ${yr}${mmdd}.ocean_cobalt_daily_2d.nc ${simname}_selected_daily_${yr}${mmdd}.nc
+        # rm ${yr}${mmdd}.ocean_cobalt_daily_2d.nc
+
+        # # Ice daily
+
+        # echo "   extracting ice variables..."
+        # tar -xf $arch_dir/${yr}${mmdd}.nc.tar ./${yr}${mmdd}.ice_daily.nc
+        # ncrename -d xT,ih -d yT,jh ./${yr}${mmdd}.ice_daily.nc # renaming ice dimensions for easier processing later
+        # ncks -F -A -d ih,${x1},${x2} -d jh,${y1},${y2} -v ${vari} ${yr}${mmdd}.ice_daily.nc ${simname}_selected_daily_${yr}${mmdd}.nc
+        # rm ${yr}${mmdd}.ice_daily.nc
+
+        # Move to globus untrusted endopoint staging area
+        
+        # echo "   moving to staging folder..."
+
+        # mv ${simname}_selected_daily_${yr}${mmdd}.nc $transferfol
+
+done
