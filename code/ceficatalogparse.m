515482388e08ca63e948ec0d5f7bc47ebf63f95e
Kelly Kearney <kelly.kearney@noaa.gov>
2026-04-30 22:36:17 -0700

    Built site for gh-pages

--- code/ceficatalogparse.m
+++
@@ -1,8 +0,0 @@
-function ceficatalogparse(pth)
-
-arguments
-    pth {mustBeTextScalar} ="http://psl.noaa.gov/thredds/catalog/Projects/CEFI/regional_mom6/cefi_portal/"
-end
-
-tree = parseXML(fullfile(pth, 'catalog.xml');
-
