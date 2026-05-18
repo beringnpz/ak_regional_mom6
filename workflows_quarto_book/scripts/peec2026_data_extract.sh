d415251efb93abe8f7b10d754272263fce7c996c
Kelly Kearney <kelly.kearney@noaa.gov>
2026-05-18 14:55:10 -0700

    peec 2026 wip

--- workflows_quarto_book/scripts/peec2026_data_extract.sh
+++ workflows_quarto_book/scripts/peec2026_data_extract.sh
@@ -66,3 +66,24 @@ calculate_clim_anom.sh --region nep \
                        --exptype hcast \
                        --ppdir /work/Kelly.Kearney/ak_cefiportal \
                        --vars tos,tob,btm_o2,btm_co3_sol_arag,btm_htotal,btm_co3_ion,pco2surf,siconc
+
+# Calculate persistence forecast
+
+calculate_persis_forecast.sh --region nep \
+                       --subdomain iq0-342jq446-743 \
+                       --release e202604 \
+                       --exptype hcast \
+                       --ppdir /work/Kelly.Kearney/ak_cefiportal \
+                       --vars tos,tob,btm_o2,btm_co3_sol_arag,btm_htotal,btm_co3_ion,pco2surf,siconc \
+                       --filedatestr 20260101 \
+                       --tindex 120
+
+# Calculate survey region averages
+
+calculate_surveyregion_averages.sh --region nep \
+                       --subdomain iq0-342jq446-743 \
+                       --release e202604 \
+                       --exptype hcast \
+                       --ppdir /work/Kelly.Kearney/ak_cefiportal \
+                       --maskbase ak_masks \
+                       --maskdatestr 20240101
